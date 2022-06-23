604f23f932fa932d652e272b62b2b0311861caa7
GabrielMelo01 <borges.gm22@gmail.com>
2022-06-23 19:48:40 -0300

    criando repository e service para categoria

--- src/main/java/com/gm_digital/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/gm_digital/cursomc/resources/CategoriaResource.java
@@ -2,28 +2,28 @@ package com.gm_digital.cursomc.resources;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gm_digital.cursomc.domain.Categoria;
+import com.gm_digital.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value ="/categorias")
 public class CategoriaResource {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<Categoria> listar(){
+    @Autowired
+    private CategoriaService service;
 
-        Categoria cat1 = new Categoria(1,"Informatica");
-        Categoria cat2 = new Categoria(2,"Escritorio");
-
-        List<Categoria> lista = new ArrayList<>();
-        lista.add(cat1);
-        lista.add(cat2);
-
-        return lista;
+    @RequestMapping(value ="/{id}",method = RequestMethod.GET)
+    public ResponseEntity<Optional<?>> find(@PathVariable Integer id){
+        return ResponseEntity.ok(service.findById(id));
     }
-    
+     
 }
---
+++ src/main/java/com/gm_digital/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.gm_digital.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gm_digital.cursomc.domain.Categoria;
+import com.gm_digital.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repository;
+
+    public Optional<Categoria> findById(Integer id) {
+        return repository.findById(id);
+    }
+
+}

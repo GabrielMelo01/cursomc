7f69b6ee946d0b8b5277a762ebd14704fc8fdb1f
gabriel <gabrielmelo@ingressolive.com>
2023-02-10 15:24:30 -0300

    item pedido e item-pedido PK

---
+++ src/main/java/com/gm_digital/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,10 @@
+package com.gm_digital.cursomc.repositories;
+
+import com.gm_digital.cursomc.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+    
+}

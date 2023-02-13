33b03fddbba759508a103193481e7ae478a0fb53
gabriel <gabrielmelo@ingressolive.com>
2023-02-13 13:39:00 -0300

    personalizando o retorno da validação

--- src/main/java/com/gm_digital/cursomc/resources/exeptions/ResourceExceptionHandler.java
+++ src/main/java/com/gm_digital/cursomc/resources/exeptions/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import com.gm_digital.cursomc.services.exeptions.DataIntegrityExeption;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -21,6 +22,13 @@ public class ResourceExceptionHandler {
     @ExceptionHandler(DataIntegrityExeption.class)
     public ResponseEntity<StandardError> dataIntegrity(DataIntegrityExeption e, HttpServletRequest request){
         StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+        ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+        e.getBindingResult().getFieldErrors().forEach(x -> err.addError(x.getField(), x.getDefaultMessage()));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }

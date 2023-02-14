9bf01023eb9c40114bdc1683e8d97c4f0d443819
gabriel <gabrielmelo@ingressolive.com>
2023-02-14 15:25:20 -0300

    inserindo novo cliente

--- src/main/java/com/gm_digital/cursomc/resources/ClienteResource.java
+++ src/main/java/com/gm_digital/cursomc/resources/ClienteResource.java
@@ -2,6 +2,7 @@ package com.gm_digital.cursomc.resources;
 
 import com.gm_digital.cursomc.domain.Cliente;
 import com.gm_digital.cursomc.dto.ClienteDTO;
+import com.gm_digital.cursomc.dto.ClienteNewDTO;
 import com.gm_digital.cursomc.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -31,13 +32,13 @@ public class ClienteResource {
         return ResponseEntity.ok().body(obj);
     }
 
-//    @RequestMapping(method = RequestMethod.POST)
-//    public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDto){
-//        Cliente obj = service.fromDTO(objDto);
-//        obj = service.insert(obj);
-//        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-//        return ResponseEntity.created(uri).build();
-//    }
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){
+        Cliente obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
 
     @RequestMapping(value ="/{id}",method = RequestMethod.PUT)
     public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
--- src/main/java/com/gm_digital/cursomc/services/ClienteService.java
+++ src/main/java/com/gm_digital/cursomc/services/ClienteService.java
@@ -1,9 +1,13 @@
 package com.gm_digital.cursomc.services;
 
+import com.gm_digital.cursomc.domain.Cidade;
 import com.gm_digital.cursomc.domain.Cliente;
-import com.gm_digital.cursomc.domain.Cliente;
+import com.gm_digital.cursomc.domain.Endereco;
+import com.gm_digital.cursomc.domain.enums.TipoCliente;
 import com.gm_digital.cursomc.dto.ClienteDTO;
+import com.gm_digital.cursomc.dto.ClienteNewDTO;
 import com.gm_digital.cursomc.repositories.ClienteRepository;
+import com.gm_digital.cursomc.repositories.EnderecoRepository;
 import com.gm_digital.cursomc.services.exeptions.DataIntegrityExeption;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -20,8 +24,18 @@ public class ClienteService {
     @Autowired
     private ClienteRepository repository;
 
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
     public Optional<Cliente> findById(Integer id) {
         return repository.findById(id);
+    }
+
+    public Cliente insert(Cliente obj) {
+        obj.setId(null);
+        obj = repository.save(obj);
+        enderecoRepository.saveAll(obj.getEnderecos());
+        return obj;
     }
 
     public Cliente update(Cliente obj) {
@@ -51,6 +65,23 @@ public class ClienteService {
 
     public Cliente fromDTO(ClienteDTO objDto) {
         return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+    }
+
+    public Cliente fromDTO(ClienteNewDTO objDto) {
+        Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+        Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+        Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+
+        cli.getEnderecos().add(end);
+        cli.getTelefones().add(objDto.getTelefone1());
+        if (objDto.getTelefone2()!=null) {
+            cli.getTelefones().add(objDto.getTelefone2());
+        }
+        if (objDto.getTelefone3()!=null) {
+            cli.getTelefones().add(objDto.getTelefone3());
+        }
+        return cli;
+
     }
 
     private void updateData(Cliente newObj, Cliente obj) {

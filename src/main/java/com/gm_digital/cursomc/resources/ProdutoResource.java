3f63cd67289e2326240933212a98ca074b8f4430
gabriel <gabrielmelo@ingressolive.com>
2023-02-15 14:31:38 -0300

    busca de produtos

--- src/main/java/com/gm_digital/cursomc/resources/ProdutoResource.java
+++ src/main/java/com/gm_digital/cursomc/resources/ProdutoResource.java
@@ -1,7 +1,13 @@
 package com.gm_digital.cursomc.resources;
 
+import com.gm_digital.cursomc.domain.Categoria;
+import com.gm_digital.cursomc.domain.Produto;
+import com.gm_digital.cursomc.dto.CategoriaDTO;
+import com.gm_digital.cursomc.dto.ProdutoDTO;
+import com.gm_digital.cursomc.resources.utils.URL;
 import com.gm_digital.cursomc.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,14 +21,27 @@ public class ProdutoResource {
     @Autowired
     private ProdutoService service;
 
-    @GetMapping
-    public ResponseEntity<List<?>> findAll(){
-        return ResponseEntity.ok(service.findAll());
-    }
-
-    @GetMapping(value ="/id/{id}")
+    @GetMapping(value ="/{id}")
     @ResponseBody
     public ResponseEntity<Optional<?>> findById(@RequestParam Integer id) {
         return ResponseEntity.ok(service.findById(id));
     }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<Page<ProdutoDTO>> findPage(
+            @RequestParam(value = "nome", defaultValue = "") String nome,
+            @RequestParam(value = "categorias", defaultValue = "") String categorias,
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction
+    ){
+        String nomeDecoded = URL.decodeParam(nome);
+        List<Integer> ids = URL.decodeIntList(categorias);
+        Page<Produto> list = service.search(nomeDecoded, ids, page, linesPerPage, orderBy, direction);
+        Page<ProdutoDTO> listDto = list.map(obj -> new ProdutoDTO(obj));
+        return ResponseEntity.ok().body(listDto);
+    }
+
+
 }
--- src/main/java/com/gm_digital/cursomc/services/ProdutoService.java
+++ src/main/java/com/gm_digital/cursomc/services/ProdutoService.java
@@ -1,8 +1,13 @@
 package com.gm_digital.cursomc.services;
 
+import com.gm_digital.cursomc.domain.Categoria;
 import com.gm_digital.cursomc.domain.Produto;
+import com.gm_digital.cursomc.repositories.CategoriaRepository;
 import com.gm_digital.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,11 +19,20 @@ public class ProdutoService {
     @Autowired
     private ProdutoRepository repository;
 
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
     public List<Produto> findAll(){
         return repository.findAll();
     }
 
     public Optional<Produto> findById(Integer id){
         return repository.findById(id);
+    }
+
+    public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        List<Categoria> categorias = categoriaRepository.findAllById(ids);
+        return repository.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
     }
 }

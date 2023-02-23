aacf6cc5bcf240c3b8b7b7485c724ee265956c9b
gabriel <gabrielmelo@ingressolive.com>
2023-02-23 10:43:25 -0300

    salvando senha com encode

--- src/main/java/com/gm_digital/cursomc/services/DBService.java
+++ src/main/java/com/gm_digital/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import com.gm_digital.cursomc.domain.enums.EstadoPagamento;
 import com.gm_digital.cursomc.domain.enums.TipoCliente;
 import com.gm_digital.cursomc.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.text.ParseException;
@@ -31,6 +32,9 @@ public class DBService {
     PagamentoRepository pagamentoRepository;
     @Autowired
     ItemPedidoRepository itemPedidoRepository;
+
+    @Autowired
+    BCryptPasswordEncoder pe;
 
     public void instantiateTestDatabase() throws ParseException {
         Categoria cat1 = new Categoria(null, "Informatica");
@@ -81,7 +85,7 @@ public class DBService {
         estadoRepository.saveAll(Arrays.asList(est1, est2));
         cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-        Cliente cli1 = new Cliente(null, "Maria Silva", "gabrielmelo@ingressolive.com", "36378912377", TipoCliente.PESSOAFISICA);
+        Cliente cli1 = new Cliente(null, "Maria Silva", "gabrielmelo@ingressolive.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 
         cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 
